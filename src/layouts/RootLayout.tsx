bfa917eb3395b685af4146ffdff5f598d8b53b83
Can Kologlu <mckologlu@gmail.com>
2023-05-30 13:47:42 -0400

    add grid to cards

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,16 +1,28 @@
 import { Outlet } from "react-router-dom";
 import React from "react";
 import Navbar from "../components/Navbar"
+import { Grid, GridItem } from "@chakra-ui/react";
 
 export default function RootLayout () {
   return(
-    <>
-  
-  <div>
-    <Navbar/>
-    <Outlet />
-  </div>
-  <div>hey there</div>
-  </>)
+  <Grid templateColumns="repeat(6, 1fr)">
+    <GridItem
+    as="aside"
+    colSpan={1}
+    bg="blackAlpha.200"
+    minHeight="100vh"
+    p="20px"
+    >
+      <span>sidebar here</span>
+    </GridItem>
+    <GridItem
+    as="main"
+    colSpan={5}
+    >
+      <Navbar/>
+      <Outlet />
+    </GridItem>
+  </Grid>
+  )
   
 }
--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -3,7 +3,7 @@ import { Box, SimpleGrid } from "@chakra-ui/react";
 
 export default function Cards() {
   return (
-    <SimpleGrid minChildWidth="250px" spacing="10px">
+    <SimpleGrid p="10px" minChildWidth="250px" spacing="10px">
       <Box bg="green.300" h="200px" border="2px solid">chandra</Box>
       <Box bg="green.300" h="200px" border="2px solid">jace</Box>
       <Box bg="green.300" h="200px" border="2px solid">ajani</Box>
@@ -16,8 +16,6 @@ export default function Cards() {
       <Box bg="green.300" h="200px" border="2px solid">chandra</Box>
       <Box bg="green.300" h="200px" border="2px solid">jace</Box>
       <Box bg="green.300" h="200px" border="2px solid">ajani</Box>
-      
-
     </SimpleGrid>
   )
 }
