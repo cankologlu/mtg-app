3ce3d45a632056d61c357a69fb47edd4d699b4b0
Can Kologlu <mckologlu@gmail.com>
2023-06-07 00:33:39 -0400

    fix-latency add-list

--- src/components/SetsMenu.tsx
+++ src/components/SetsMenu.tsx
@@ -8,11 +8,13 @@ import {
   MenuGroup,
   MenuOptionGroup,
   MenuDivider,
-  list,
+  List,
   Heading,
+  ListItem,
 } from "@chakra-ui/react";
 import { Box, Button, Grid, GridItem, Image, Link } from "@chakra-ui/react";
 import { CardSet } from "../types/Cardset";
+import { NavLink } from "react-router-dom";
 
 type SetsMenuProps = {
   sets: {
@@ -27,51 +29,50 @@ export default function SetsMenu (props: SetsMenuProps ) {
  
   return (
 
-    <Menu isOpen={true} >
-      <Box position={"relative"}
-      className="horse"
-        bg=""
-        css={{
-          "&::-webkit-scrollbar": {
-            width: "4px",
-          },
-          "&::-webkit-scrollbar-track": {
-            width: "6px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            background: "#8ccef0",
-            borderRadius: "24px",
-          },
-        }}
-        overflowX="auto"
-        maxHeight="600px"
-      >
+    <Box position={"relative"}
+    className="horse"
+    bg="#FAD7A0"
+    css={{
+      "&::-webkit-scrollbar": {
+        width: "4px",
+      },
+      "&::-webkit-scrollbar-track": {
+        width: "px",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        background: "#8ccef0",
+        borderRadius: "24px",
+      },
+    }}
+    overflowX="auto"
+    maxHeight="600px"
+    >
+        <List>
   {Object.entries(sets.data).map(([set_type, sets]) => (
-    <MenuList key={`panda${Math.random()*10}`}>
-   {set_type !== "vanguard" && <MenuButton as={Heading} key={set_type} w="200px">
+   <Box as={Heading} key={set_type} w="200px" fontSize="h4" p={4} >
     {set_type}
-    </MenuButton>}
-        {sets.filter((set: CardSet) => set.name !== "Magic Online Avatars" && set.name !== "Vanguard Series")
-        .map((set: CardSet) => (
-          <MenuItem position={"relative"}
-            key={set.id}
-            maxW="120px"
-            fontSize={"x-small"}
-            transform={"none"}
-          >
+        {sets.map((set: CardSet) => (
+          <ListItem 
+          position={"relative"}
+          key={set.id}
+          maxW="120px"
+          fontSize={"x-small"}
+          transform={"none"}
+          ><NavLink to={`/sets/${set.code}`}>
+            {set.name}
             <Image
               src={set.icon_svg_uri}
               alt={set.name}
               boxSize="2rem"
               borderRadius="full"
               mr="12px"
-            />
-            {set.name}
-          </MenuItem>
-          ))}
-      </MenuList>
+              />
+          </NavLink>
+          </ListItem>
+    ))}
+    </Box>
   ))}
+    </List>
   </Box>
-    </Menu>
   )
 }
