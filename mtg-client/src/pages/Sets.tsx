4fbcc6635db319a2645b1ed06a62a6ba599063bb
Can Kologlu <mckologlu@gmail.com>
2023-07-04 16:26:54 -0400

    sets-page-css

--- mtg-client/src/pages/Sets.tsx
+++ mtg-client/src/pages/Sets.tsx
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from "react";
-import {
-  List,
-  ListItem,
-  Text,
-} from "@chakra-ui/react";
+import { Flex, HStack, List, ListItem, Text } from "@chakra-ui/react";
 import { Box, Grid, GridItem, Image, Link } from "@chakra-ui/react";
 import { CardSet } from "../types/Cardset";
 import { NavLink } from "react-router-dom";
 import getAllSets from "../helpers/getAllSets";
-
-
-
 
 export default function Sets() {
   const [sets, setSets] = useState<{
@@ -22,37 +15,35 @@ export default function Sets() {
     has_more: false,
     data: {},
   });
-  
-useEffect(() => {
-  getAllSets()
-    .then((data) => {
-      // Group the sets data by set_type
-      console.log("data from getAllSets", data)
-      const groupedSets = data.data.reduce(
-        (acc: { [key: string]: CardSet[] }, set: CardSet) => {
-          const set_type = set.set_type;
-          if (acc[set_type]) {
-            acc[set_type].push(set);
-          } else {
-            acc[set_type] = [set];
-          }
-          return acc;
-        },
-        {}
-      );
-      // Update the state with the grouped sets data
-      setSets({
-        object: data.object,
-        has_more: data.has_more,
-        data: groupedSets,
-      });
-      console.log("sets from useEFfect:",sets)
-    })
-    .catch((error) => console.log(`ERROR is ${error}`));
-}, []);
 
- 
-  
+  useEffect(() => {
+    getAllSets()
+      .then((data) => {
+        // Group the sets data by set_type
+        console.log("data from getAllSets", data);
+        const groupedSets = data.data.reduce(
+          (acc: { [key: string]: CardSet[] }, set: CardSet) => {
+            const set_type = set.set_type;
+            if (acc[set_type]) {
+              acc[set_type].push(set);
+            } else {
+              acc[set_type] = [set];
+            }
+            return acc;
+          },
+          {}
+        );
+        // Update the state with the grouped sets data
+        setSets({
+          object: data.object,
+          has_more: data.has_more,
+          data: groupedSets,
+        });
+        console.log("sets from useEFfect:", sets);
+      })
+      .catch((error) => console.log(`ERROR is ${error}`));
+  }, []);
+
   type SetsMenuProps = {
     sets: {
       object: string;
@@ -70,7 +61,7 @@ useEffect(() => {
     <Box
       position={"relative"}
       className="horse"
-      bg="blue.200"
+      bg="orange.200"
       borderRadius={"l"}
       css={{
         "&::-webkit-scrollbar": {
@@ -85,15 +76,14 @@ useEffect(() => {
         },
       }}
       overflowX="auto"
-      maxHeight="600px"
+      maxHeight="100vh"
     >
       <List>
-        
         {Object.entries(sets.data).map(([set_type, sets]) => (
           <Box
-            as={Text}
+            as={Grid}
             key={set_type}
-            w="200px"
+            w="auto"
             fontSize="xl"
             p={4}
             fontWeight={600}
@@ -105,7 +95,7 @@ useEffect(() => {
             {sets.map((set: CardSet) => (
               <ListItem
                 display={"grid"}
-                gridAutoFlow="row dense"
+                // gridAutoFlow="column dense"
                 position={"relative"}
                 key={set.id}
                 width={"auto"}
@@ -113,20 +103,28 @@ useEffect(() => {
                 transform={"none"}
               >
                 <NavLink to={`/cards/${set.code}`}>
-                  <Box as={Grid}>
-                    {/* <SkeletonCircle isLoaded={isLoaded}> */}
-                    <Image
-                    //  onLoad={() => loaded()}
-                      fallbackSrc="https://www.seekpng.com/png/full/24-240917_unicorn-unicorns-emoji-emoji-horse-freetoedit-unicorn-emoji.png"
-                      src={set.icon_svg_uri}
-                      alt={set.name}
-                      boxSize="2rem"
-                      borderRadius="full"
-                      mr="12px"
-                    />
-                    {/* </SkeletonCircle> */}
-                    {set.name}
-                  </Box>
+                  <Flex alignItems="center" justifyContent={"space-between"}>
+                    <Box m="12px">
+                      <Image
+                        fallbackSrc="https://www.seekpng.com/png/full/24-240917_unicorn-unicorns-emoji-emoji-horse-freetoedit-unicorn-emoji.png"
+                        src={set.icon_svg_uri}
+                        alt={set.name}
+                        boxSize="2rem"
+                        borderRadius="full"
+                      />
+                    </Box>
+                    <Box>
+                      <Text>{set.name}</Text>
+                    </Box>
+                    <Flex>
+                      <Box ml="auto">
+                        <Text>{`${set.card_count} Cards`}</Text>
+                        </Box>
+                        <Box>
+                          <Text>{`Release Date: ${set.released_at}`}</Text>
+                        </Box>
+                    </Flex>
+                    </Flex>
                 </NavLink>
               </ListItem>
             ))}
